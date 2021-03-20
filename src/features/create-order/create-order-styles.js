7d6ac49105f5286e6eec3db73a65a3e4540f46a2
Sayan Sivaneswaran <bredx2000@gmail.com>
2021-03-20 15:20:43 +0100

    refactor(design): remove material ui  fully

--- src/features/create-order/create-order-styles.js
+++
@@ -1,8 +0,0 @@
-import { Button } from '@material-ui/core';
-import { rem } from 'polished';
-import styled from 'styled-components';
-
-export const StyledButton = styled(Button)`
-  margin-top: ${rem(20)} !important;
-  margin-bottom: ${rem(20)}!important;
-`;
